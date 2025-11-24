1410dfcc2734697c01a51e6e00166137f623a821
Sergio-Rojas27 <srojas.1871@unimar.edu.ve>
2025-11-24 11:26:39 -0800

    Mas cosas y mas css

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-
-    <nav>
+    
+    <nav style="display: flex; justify-content: center; align-items: center;">
         <div class="head">
             <div class="brillo-logo">
                 <img src="media/gameloversLogo 2.svg" alt="Aca va el loguito, mogul" class="logo-head">
@@ -18,12 +18,26 @@
     </nav>
 
     <div class="carrusel">
+        <section>
+            <div class="imagen-conoce"></div>
+            <div class="imagen-chatea"></div>
+            <div class="imagen-conecta"></div>
+        </section>
+    </div>
+    <script></script>
+    <script></script>
+    <script></script>
+
+    <p class="Subtitulos" style="text-align: center;">¿List@ para ser <br>parte de game lovers?</p>
+
+    <div class="buttons-container-center" style="margin-top: 7%;">
+        <a href="register.php"><button class="btn-verde">Registrarse</button></a>
     </div>
 
-    <p class="Subtitulos">¿List@ para ser parte de game lovers?</p>
-    <div class="buttons-container">
-        <a href="login.php"><button class="btn-atras">Atrás</button></a>
-        <button type="submit" class="btn-siguiente">Siguiente</button>
+    <p class="texto-general" style="text-align: center; margin-top: 25%;">¿Ya tienes una cuenta?</p>
+
+    <div class="buttons-container-center">
+        <a href="login.php"><button class="btn-azul">Iniciar sesión</button></a>
     </div>
 </body>
 </html>
