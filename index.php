e312d3d2a23772b421351e3d6f82af34ee471ba1
Sergio-Rojas27 <srojas.1871@unimar.edu.ve>
2025-11-24 08:30:22 -0800

    Creacion del index, y anexo de archivos

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenida</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+    <nav>
+        <div class="head">
+            <div class="brillo-logo">
+                <img src="media/gameloversLogo 2.svg" alt="Aca va el loguito, mogul" class="logo-head">
+            </div>
+            <img src="media/Titulo.svg" alt="">
+        </div>
+    </nav>
+
+    <div class="carrusel">
+    </div>
+
+    <p class="Subtitulos">¿List@ para ser parte de game lovers?</p>
+    <div class="buttons-container">
+        <a href="login.php"><button class="btn-atras">Atrás</button></a>
+        <button type="submit" class="btn-siguiente">Siguiente</button>
+    </div>
+</body>
+</html>
