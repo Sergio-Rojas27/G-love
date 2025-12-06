56ac05857bb4246026c9b996bd8332beeb19024e
Sergio-Rojas27 <srojas.1871@unimar.edu.ve>
2025-12-05 23:05:42 -0400

    registro 1/3 listo, parte visual lista y ajustes generalesde front

--- app/views/register.php
+++ app/views/register.php
@@ -7,8 +7,14 @@
         <input required type="text" id="nombre" name="nombre" class="form-input" placeholder="Nombre">
         <input required type="text" id="apellido" name="apellido" class="form-input" placeholder="Apellido">
         <input required type="text" id="correo" name="correo" class="form-input" placeholder="Correo Electrónico">
-        <input required type="password" id="password" name="password" class="form-input" placeholder="Contraseña">
-        <input required type="password" id="conf-pass" name="conf-pass" class="form-input" placeholder="Confirmar contraseña">
+        <div class="register-container">
+            <input required type="password" id="password" name="password" class="form-input" placeholder="Contraseña">
+            <i class='bx'><img id="ojo" src="media/ojoA.png" alt=""></i>
+        </div>
+        <div class="register-container">
+            <input required type="password" id="conf-pass" name="conf-pass" class="form-input" placeholder="Confirmar contraseña">
+            <i class='bx1'><img id="ojo1" src="media/ojoA.png" alt=""></i>
+        </div>
         <select required name="sexo" id="sexo" class="form-input">
             <option value="" selected disabled hidden>Sexo</option>
             <option value="masculino">Masculino</option>
@@ -20,17 +26,34 @@
             <span class="texto-general">Nacimiento: </span>
             <input required type="date" name="nacimiento" id="nacimiento">
         </section>
-        <input required type="file" name="cedula" id="cedula" accept="image/*" required>
+        <div class="buttons-container">
+            <label for="cedula">
+                <input required type="file" class="upload-button-styled" name="cedula" id="cedula" accept="image/*" required>
+                <img src="media/Btn-Upload.png" alt="">
+            </label>
+            <span id="file-name-display" class="file-name">Ningún archivo seleccionado.</span>
+        </div>
+        
+        </div>
         <!-- UY PAPA MIGUEL NO TENGO NI PUTA IDEA DE COMO HACER LO DE LA CI Y LAS IMAGENES, BUENA SUERTE -->
     </div>
     <section class="checkbox-container">
-        <input required type="checkbox" id="terminos" name="terminos" value="si">
-        <label for="terminos">Terminos y condiciones</label> <br>
-        <input required type="checkbox" id="privacidad" name="privacidad" value="si">
-        <label for="terminos">Política de privacidad</label>
+        <div class="container">
+            <input required type="checkbox" id="terminos" name="terminos" value="si">
+            <p class="texto-general">Terminos y condiciones</p>
+        </div>
+        <div class="container">
+            <input required type="checkbox" id="privacidad" name="privacidad" value="si">
+            <p class="texto-general">Política de privacidad</p>
+        </div>
     </section>
+    
     <div class="buttons-container">
+        <a href="?home"><button class="gradiente-morado">Atrás</button></a>
+
         <button type="submit" class="gradiente-verde">Siguiente</button>
+    </div> 
 </div> 
 </form>
-        <a href="?home"><button class="gradiente-morado">Atrás</button></a>
+
+<script src="script.js"></script>
