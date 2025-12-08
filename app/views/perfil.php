f0c1a2dd4d5392f3e55d0df631db5768a2fb74f5
Sergio-Rojas27 <srojas.1871@unimar.edu.ve>
2025-12-07 23:41:31 -0400

    perfiles, correciones de front y mas vainas que ya no me acuerdo

--- app/views/perfil.php
+++ app/views/perfil.php
@@ -4,10 +4,73 @@
 ?>
 <form action="" method="POST" class="form-todo">
     <div class="container-left">
-        <img src="media/search.png" alt=""  class="img-perfil" style="margin-right: 30px;">
+        <img src="media/search.png" alt=""  class="img-perfil-p">
         <div id="container-names">
-            <p class="Subtitulos">Nombre del tipo</p>
+            <p class="Subtitulos" id="NombrePerfil">Nombre del tipo</p>
             <p class="Subtitulos-gris">Nickname del tipo</p>
+        </div>
+        <div class="compartir">
+            <img src="media/share.svg" alt="">
+        </div>
+    </div>
+
+    <div class="container-center">
+        <div class="btns-perfil">
+            <p class="texto-general">Fotos</p>
+        </div>
+        <div class="btns-perfil">
+            <p class="texto-general">Información</p>
+        </div>
+        <div class="btns-perfil">
+            <p class="texto-general">Metas</p>
+        </div>
+    </div>
+
+    <p class="Subtitulos" style="text-align: center; margin-top: 1.5rem;">Fotos</p>
+
+    <div class="container-form" style="padding-left: 0;">
+                <div class="container-img">
+            <label for="im1">
+                <input required type="file" class="upload-button-styled" name="fotos[]" id="im1" accept="image/*" required>
+                <img src="media/NewImg.svg"  alt="foto" id="img1" class="img-general">
+            </label>
+            <label for="im2">
+                <input required type="file" class="upload-button-styled" name="fotos[]" id="im2" accept="image/*" required>
+                <img src="media/NewImg.svg"  alt="foto" id="img2" class="img-general">
+            </label>
+            <label for="im3">
+                <input type="file" class="upload-button-styled" name="fotos[]" id="im3" accept="image/*">
+                <img src="media/NewImg.svg" alt="foto" id="img3" class="img-general">
+            </label>
+        </div>
+        <div class="container-img">
+            <label for="im4">
+                <input type="file" class="upload-button-styled" name="fotos[]" id="im4" accept="image/*">
+                <img src="media/NewImg.svg"  alt="foto" id="img4" class="img-general">
+            </label>
+            <label for="im5">
+                <input type="file" class="upload-button-styled" name="fotos[]" id="im5" accept="image/*">
+                <img src="media/NewImg.svg"  alt="foto" id="img5" class="img-general">
+            </label>
+            <label for="im6">
+                <input type="file" class="upload-button-styled" name="fotos[]" id="im6" accept="image/*">
+                <img src="media/NewImg.svg"  alt="foto" id="img6" class="img-general">
+            </label>
+        </div>
+
+        <div class="container-img">
+            <label for="im7">
+                <input type="file" class="upload-button-styled" name="fotos[]" id="im7" accept="image/*">
+                <img src="media/NewImg.svg"  alt="foto" id="img7" class="img-general">
+            </label>
+            <label for="im8">
+                <input type="file" class="upload-button-styled" name="fotos[]" id="im8" accept="image/*">
+                <img src="media/NewImg.svg"  alt="foto" id="img8" class="img-general">
+            </label>
+            <label for="im9">
+                <input type="file" class="upload-button-styled" name="fotos[]" id="im9" accept="image/*">
+                <img src="media/NewImg.svg"  alt="foto" id="img9" class="img-general">
+            </label>
         </div>
     </div>
 
@@ -17,3 +80,4 @@
     $pagina = '4';
     require_once __DIR__ . '../../templates/footer.php';
 ?>
+<script src="fotos.js"></script>
---
+++ app/views/perfil2.php
@@ -0,0 +1,96 @@
+<?php 
+    $titulo_header = 'Game Lovers';
+    require_once __DIR__ . '../../templates/header.php';
+?>
+<form action="" method="POST" class="form-todo">
+    <div class="container-left">
+        <img src="media/search.png" alt=""  class="img-perfil-p">
+        <div id="container-names">
+            <p class="Subtitulos" id="NombrePerfil">Nombre del tipo</p>
+            <p class="Subtitulos-gris">Nickname del tipo</p>
+        </div>
+        <div class="compartir">
+            <img src="media/share.svg" alt="">
+        </div>
+    </div>
+
+    <div class="container-center">
+        <div class="btns-perfil">
+            <p class="texto-general">Fotos</p>
+        </div>
+        <div class="btns-perfil">
+            <p class="texto-general">Información</p>
+        </div>
+        <div class="btns-perfil">
+            <p class="texto-general">Metas</p>
+        </div>
+    </div>
+
+    <p class="Subtitulos" style="text-align: center; margin-top: 1.5rem;">Información General</p>
+
+    <div class="container-form">
+        <input required type="text" id="nombre" name="nombre" class="form-input" placeholder="Nombre">
+        <input required type="text" id="apellido" name="apellido" class="form-input" placeholder="Apellido">
+        <div class="register-container">
+            <input required type="password" id="correo" name="correo" class="form-input" placeholder="Correo Electrónico">
+            <i class='bx'><img id="ojo" src="media/ojoA.png" alt=""></i>
+        </div>
+        <select required name="sexo" id="sexo" class="form-input">
+            <option value="" selected disabled hidden>Género</option>
+            <option value="1">Hombre</option>
+            <option value="2">Mujer</option>
+            <option value="3">Otro</option>
+            <option value="4">Prefiero no decirlo</option>
+        </select>
+        <div class="container-between">
+        <select required name="orientacion" id="genero-fav" class="form-input-40">
+            <option value="" selected disabled hidden>Orientación</option>
+            <option value="1">Heterosexual</option>
+            <option value="2">Homosexual</option>
+            <option value="3">Bisexual</option>
+            <option value="4">Otro</option>
+        </select>
+
+        <select required name="busco" id="genero-fav" class="form-input-40">
+            <option value="" selected disabled hidden>Busco</option>
+            <option value="60">Amigos</option>
+            <option value="62">Jugar</option>
+            <option value="61">Relación seria</option>
+            <option value="63">Relación casual</option>
+        </select>
+        </div>
+        <div class="file-container">
+            <label for="ubicacion">
+                <input required type="file" class="upload-button-styled" name="ubicacion" id="ubicacion" accept="image/*" required>
+                <img src="media/mapa.svg" alt="">
+            </label>
+            <input type="text" class="input-sin-fondo" placeholder="Tu Ubicacion">
+        </div>
+    </div>
+    
+    <p class="Subtitulos" style="text-align: center; margin-top: 1.5rem;">Juegos y Cualidades</p>
+
+    <div class="container-form">
+        <div class="register-container">
+            <input type="text" class="form-input" placeholder="Buscar Juegos">
+            <i class='icono'><img id="search1" src="media/search.png" alt=""></i>
+        </div>
+
+        <div class="register-container">
+            <input type="text" class="form-input" placeholder="Buscar Juegos">
+            <i class='icono'><img id="search1" src="media/search.png" alt=""></i>
+        </div>
+    </div>
+
+    <div class="button-container">
+        <button class="gradiente-morado button-fake" id="editar-perfil">Editar Perfil</button>
+        <button class="gradiente-morado button-fake" id="guardar-perfil">Editar Perfil</button>
+    </div> 
+
+</form>
+
+<?php 
+    $pagina = '4';
+    require_once __DIR__ . '../../templates/footer.php';
+?>
+<script src="fotos.js"></script>
